f4261c582c7928d57d84e431074e5a06318b4ca7
Fredrick Oluoch <menty44@gmail.com>
2023-03-04 14:54:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+import { LocalNotifications} from '@ionic-native/local-notifications/ngx'
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BarcodeScanner, LocalNotifications],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
+
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  scannedData: any;
+  encodedData!: '';
+  encodeData: any;
+  inputData: any;
+
+  constructor(private barcodeScanner: BarcodeScanner) { }
+
+  scanBarcode() {
+    const options: BarcodeScannerOptions = {
+      preferFrontCamera: false,
+      showFlipCameraButton: true,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: 'Place a barcode inside the scan area',
+      resultDisplayDuration: 500,
+      formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
+      orientation: 'portrait',
+    };
+
+    this.barcodeScanner.scan(options).then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.scannedData = barcodeData;
+
+      alert('Success ' + JSON.stringify(barcodeData.text));
+
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+
+  createBarcode() {
+    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, this.inputData).then((encodedData) => {
+      console.log(encodedData);
+      this.encodedData = encodedData;
+    }, (err) => {
+      console.log('Error occured : ' + err);
+    });
+  }
 
 }
